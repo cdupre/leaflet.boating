14f9072ddb1f557d588f8275e8c93d0a85d556b0
Clément Dupré <clement.dupre@gmail.com>
2024-10-05 03:43:03 +0200

    mieux

--- L.Control.Boating.js
+++ L.Control.Boating.js
@@ -177,10 +177,15 @@ L.Control.Boating = L.Control.extend({
     }
   },
 
+  circleUpdate(e) {
+    this.circle.setRadius(e.accuracy)
+    this.circle.setLatLng(e.latlng)
+  },
+
   boatUpdate(e) {
     const heading = e.heading || this.lastHeading || 0
     let html = '<svg transform="rotate(' + heading + ')" viewBox="0 0 512 512"> xmlns="http://www.w3.org/2000/svg"'
-    html += '<path d="M 384 492 128 492 C 128 492 128 148 256 20 C 384 148 384 507 384 507" fill="#3388ff"/>'
+    html += '<path d="M 384 512 128 512 C 128 512 128 128 256 0 C 384 128 384 512 384 512" fill="#3388ff"/>'
     html += '</svg>'
     const icon = L.divIcon({
       className: 'boat',
@@ -190,11 +195,6 @@ L.Control.Boating = L.Control.extend({
     })
     this.boat.setLatLng(e.latlng)
     this.boat.setIcon(icon)
-  },
-
-  circleUpdate(e) {
-    this.circle.setRadius(e.accuracy)
-    this.circle.setLatLng(e.latlng)
   },
 
   lineUpdate(e = this.lastPosition) {
