d395548181bdc55343f92cb3cd9a12b1c5f34d19
clem <clement.dupre@gmail.com>
2026-08-31 01:10:33 +0200

    refactor

--- dist/L.Control.Boating.js
+++ dist/L.Control.Boating.js
@@ -5,6 +5,18 @@
 
     const { Control, DomUtil, DomEvent, Marker, DivIcon, Circle, Polyline, LatLng, Util } = L;
 
+    function cosD(deg) {
+      return Math.cos(deg * Math.PI / 180)
+    }
+
+    function sinD(deg) {
+      return Math.sin(deg * Math.PI / 180)
+    }
+
+    function atan2D(x, y) {
+      return ((Math.atan2(x, y) * 180 / Math.PI) + 360) % 360
+    }
+
     return Control.extend({
 
       options: {
@@ -13,7 +25,7 @@
         circleColor: '#3388ff',
         lineColor1: '#ffcc00',
         lineColor2: '#3388ff',
-        cacheLength: 4,
+        motionCacheLength: 4,
         legendPosition: 'bottomright',
         legendHTML: `
         <table>
@@ -70,20 +82,21 @@
       onAdd: function (map) {
         const container = DomUtil.create('div', 'leaflet-bar leaflet-control');
         const link = DomUtil.create('a', 'leaflet-bar-part leaflet-bar-part-single', container);
-        this.icon = DomUtil.create('span', 'leaflet-control-boating-arrow', link);
+        this._icon = DomUtil.create('span', 'leaflet-control-boating-arrow', link);
         link.href = '#';
 
+        DomEvent.disableClickPropagation(container);
         DomEvent.on(link, 'click', function (e) {
           DomEvent.stopPropagation(e);
           DomEvent.preventDefault(e);
-          this.onClick();
+          this._onClick();
         }, this);
 
-        this.legend = new Control({
+        this._legend = new Control({
           position: this.options.legendPosition,
           css: this.options.legendCSS,
         });
-        this.legend.onAdd = function (map) {
+        this._legend.onAdd = function (map) {
           const container = DomUtil.create('div', 'leaflet-control leaflet-bar leaflet-control-boating-legend');
           container.innerHTML = `
           <style>
@@ -95,155 +108,149 @@
           return container
         };
 
-        this.boat = new Marker([0, 0], {
+        this._boat = new Marker([0, 0], {
           icon: new DivIcon({
             iconAnchor: [12.5, 12.5],
             iconSize: [25, 25],
             className: 'boat',
             html: `
-            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" id="boat-svg">
+            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" class="boat-svg">
               <path d="M 128 512 C 128 512 128 128 256 0 C 384 128 384 512 384 512 Z" fill="${this.options.boatColor}"/>
             </svg>`,
           })
         });
-        this.boat.on('add', function() {
-          this.svg = this.getElement().querySelector('#boat-svg');
+        this._boat.on('add', function() {
+          this._svg = this.getElement().querySelector('.boat-svg');
         });
 
-        this.circle = new Circle([0, 0], {
+        this._circle = new Circle([0, 0], {
           color: this.options.circleColor,
           stroke: false,
         });
 
-        this.line = new Polyline([[0, 0], [0, 0]], {
+        this._line = new Polyline([[0, 0], [0, 0]], {
           color: this.options.lineColor2,
           lineCap: 'square',
         });
 
-        this.linebg = new Polyline([[0, 0], [0, 0]], {
+        this._linebg = new Polyline([[0, 0], [0, 0]], {
           color: this.options.lineColor1,
         });
 
         return container
       },
 
-      cosD: function (deg) {
-        return Math.cos(deg * Math.PI / 180)
+      onRemove: function() {
+        this.stop();
       },
 
-      sinD: function (deg) {
-        return Math.sin(deg * Math.PI / 180)
-      },
-
-      atan2D: function (x, y) {
-        return ((Math.atan2(x, y) * 180 / Math.PI) + 360) % 360
-      },
-
-      isRequesting: function () {
-        return this.icon.classList.contains('requesting')
-      },
-
-      isLocating: function () {
-        return this.icon.classList.contains('locating')
-      },
-
-      isFollowing: function () {
-        return this.icon.classList.contains('following')
-      },
-
-      onClick: function () {
-        if (this.isFollowing()) {
-          this.stop();
-        }
-        else if (this.isLocating()) {
-          this._map.panTo(this.lastPosition.latlng);
-          this.follow();
-        }
-        else if (!this.isRequesting()) {
-          this.request();
-        }
-      },
-
-      onDragStart: function () {
-        if (this.isFollowing()) {
-          this.unfollow();
-        }
-      },
-
-      onMoveEnd: function () {
-        if ((this.isLocating() || this.isFollowing()) && this.lastPosition) {
-          this.updateLine(this.lastPosition);
-        }
-      },
-
-      request: function () {
-        this._map.on('moveend', this.onMoveEnd, this);
-        this._map.on('dragstart', this.onDragStart, this);
-        this._map.on('locationfound', this.onLocationFound, this);
-        this._map.on('locationerror', this.onLocationError, this);
+      start: function () {
+        this._map.on('moveend', this._onMoveEnd, this);
+        this._map.on('dragstart', this._onDragStart, this);
+        this._map.on('locationfound', this._onLocationFound, this);
+        this._map.on('locationerror', this._onLocationError, this);
         this._map.locate({ watch: true, enableHighAccuracy: true });
-        this.icon.classList.remove('following');
-        this.icon.classList.remove('locating');
-        this.icon.classList.add('requesting');
-      },
-
-      follow: function () {
-        this._map.options.scrollWheelZoom = 'center';
-        this._map.options.doubleClickZoom = 'center';
-        this.icon.classList.remove('requesting');
-        this.icon.classList.remove('locating');
-        this.icon.classList.add('following');
-      },
-
-      unfollow: function () {
-        this._map.options.scrollWheelZoom = true;
-        this._map.options.doubleClickZoom = true;
-        this.icon.classList.remove('requesting');
-        this.icon.classList.remove('following');
-        this.icon.classList.add('locating');
+        this._icon.classList.remove('following');
+        this._icon.classList.remove('locating');
+        this._icon.classList.add('requesting');
+        this._motionCache = [];
+        this._lastPosition = null;
       },
 
       stop: function () {
         this._map.stopLocate();
-        this._map.off('moveend', this.onMoveEnd, this);
-        this._map.off('dragstart', this.onDragStart, this);
-        this._map.off('locationfound', this.onLocationFound, this);
-        this._map.off('locationerror', this.onLocationError, this);
+        this._map.off('moveend', this._onMoveEnd, this);
+        this._map.off('dragstart', this._onDragStart, this);
+        this._map.off('locationfound', this._onLocationFound, this);
+        this._map.off('locationerror', this._onLocationError, this);
         this._map.options.scrollWheelZoom = true;
         this._map.options.doubleClickZoom = true;
-        this.icon.classList.remove('requesting');
-        this.icon.classList.remove('following');
-        this.icon.classList.remove('locating');
-        this._map.removeControl(this.legend);
-        this._map.removeLayer(this.circle);
-        this._map.removeLayer(this.linebg);
-        this._map.removeLayer(this.line);
-        this._map.removeLayer(this.boat);
+        this._icon.classList.remove('requesting');
+        this._icon.classList.remove('following');
+        this._icon.classList.remove('locating');
+        this._map.removeControl(this._legend);
+        this._map.removeLayer(this._circle);
+        this._map.removeLayer(this._linebg);
+        this._map.removeLayer(this._line);
+        this._map.removeLayer(this._boat);
       },
 
-      onLocationFound: function (e) {
-        e.latlngDMS = this.latlngDMS(e);
-        e.smooth = this.smoothMotion(e);
+      _isRequesting: function () {
+        return this._icon.classList.contains('requesting')
+      },
 
-        if (this.isRequesting()) {
-          this._map.addControl(this.legend);
-          this._map.addLayer(this.circle);
-          this._map.addLayer(this.linebg);
-          this._map.addLayer(this.line);
-          this._map.addLayer(this.boat);
-          this.follow();
+      _isLocating: function () {
+        return this._icon.classList.contains('locating')
+      },
+
+      _isFollowing: function () {
+        return this._icon.classList.contains('following')
+      },
+
+      _onClick: function () {
+        if (this._isFollowing()) {
+          this.stop();
         }
-        if (this.isFollowing()) {
+        else if (this._isLocating()) {
+          this._map.panTo(this._lastPosition.latlng);
+          this._follow();
+        }
+        else if (!this._isRequesting()) {
+          this.start();
+        }
+      },
+
+      _onDragStart: function () {
+        if (this._isFollowing()) {
+          this._unfollow();
+        }
+      },
+
+      _onMoveEnd: function () {
+        if ((this._isLocating() || this._isFollowing()) && this._lastPosition) {
+          this._updateLine(this._lastPosition);
+        }
+      },
+
+      _follow: function () {
+        this._map.options.scrollWheelZoom = 'center';
+        this._map.options.doubleClickZoom = 'center';
+        this._icon.classList.remove('requesting');
+        this._icon.classList.remove('locating');
+        this._icon.classList.add('following');
+      },
+
+      _unfollow: function () {
+        this._map.options.scrollWheelZoom = true;
+        this._map.options.doubleClickZoom = true;
+        this._icon.classList.remove('requesting');
+        this._icon.classList.remove('following');
+        this._icon.classList.add('locating');
+      },
+
+      _onLocationFound: function (e) {
+        e.latlngDMS = this._latlngDMS(e);
+        e.smooth = this._smoothMotion(e);
+
+        if (this._isRequesting()) {
+          this._map.addControl(this._legend);
+          this._map.addLayer(this._circle);
+          this._map.addLayer(this._linebg);
+          this._map.addLayer(this._line);
+          this._map.addLayer(this._boat);
+          this._follow();
+        }
+        if (this._isFollowing()) {
           this._map.panTo(e.latlng);
         }
-        this.updateLegend(e);
-        this.updateCircle(e);
-        this.updateLine(e);
-        this.updateBoat(e);
-        this.lastPosition = e;
+        this._updateLegend(e);
+        this._updateCircle(e);
+        this._updateLine(e);
+        this._updateBoat(e);
+        this._lastPosition = e;
       },
 
-      onLocationError: function (e) {
+      _onLocationError: function (e) {
         console.error(e);
         if (e.code === 1) {
           alert('unlock geolocation please');
@@ -251,18 +258,18 @@
         }
       },
 
-      updateCircle: function (e) {
-        this.circle.setLatLng(e.latlng);
-        this.circle.setRadius(e.accuracy);
+      _updateCircle: function (e) {
+        this._circle.setLatLng(e.latlng);
+        this._circle.setRadius(e.accuracy);
       },
 
-      updateBoat: function (e) {
+      _updateBoat: function (e) {
         const heading = e.smooth.heading;
-        this.boat.svg.style.transform = 'rotate(' + heading + 'deg)';
-        this.boat.setLatLng(e.latlng);
+        this._boat._svg.style.transform = 'rotate(' + heading + 'deg)';
+        this._boat.setLatLng(e.latlng);
       },
 
-      updateLine: function (e) {
+      _updateLine: function (e) {
         const zoom = this._map.getZoom();
         const mapBounds = this._map.getBounds();
         const heading = e.smooth.heading;
@@ -276,26 +283,26 @@
         );
         const lengthDeg = length * 360 / 40000000;
         const dirPoint = new LatLng(
-          e.latlng.lat + (lengthDeg * this.cosD(heading)),
-          e.latlng.lng + (lengthDeg * this.sinD(heading) / this.cosD(e.latlng.lat)),
+          e.latlng.lat + (lengthDeg * cosD(heading)),
+          e.latlng.lng + (lengthDeg * sinD(heading) / cosD(e.latlng.lat)),
         );
-        this.line.setLatLngs([e.latlng, dirPoint]);
-        this.linebg.setLatLngs([e.latlng, dirPoint]);
+        this._line.setLatLngs([e.latlng, dirPoint]);
+        this._linebg.setLatLngs([e.latlng, dirPoint]);
 
-        const metersPerPixel = 40000000 * this.cosD(e.latlng.lat) / (256 * Math.pow(2, zoom));
+        const metersPerPixel = 40000000 * cosD(e.latlng.lat) / (256 * Math.pow(2, zoom));
         const pixelsPerHour = speed / metersPerPixel * 3600;
-        this.line.setStyle({
+        this._line.setStyle({
           dashArray: pixelsPerHour + ',' + pixelsPerHour,
           dashOffset: pixelsPerHour,
         });
       },
 
-      updateLegend: function (e) {
+      _updateLegend: function (e) {
         const nautic = 40000 / 360 / 60;
         const heading = Math.round(e.smooth.heading);
         const speed = Math.round(e.smooth.speed * 36 / nautic) / 10;
 
-        this.legend.body.innerHTML = Util.template(
+        this._legend.body.innerHTML = Util.template(
           this.options.legendHTML, {
             lineColor1: this.options.lineColor1,
             lineColor2: this.options.lineColor2,
@@ -307,7 +314,7 @@
         );
       },
 
-      latlngDMS: function (e) {
+      _latlngDMS: function (e) {
         function dms(coord) {
           let float = Math.abs(coord);
           let d = Math.floor(float);
@@ -337,25 +344,22 @@
         }
       },
 
-      smoothMotion: (function () {
-        const cache = [];
-        return function(e) {
-          cache.push(e);
-          if (cache.length > this.options.cacheLength) {
-            cache.shift();
-          }
-          const sumX = cache.reduce(
-            (sum, e) => sum + (e.speed || 0) * this.cosD(e.heading || 0), 0
-          );
-          const sumY = cache.reduce(
-            (sum, e) => sum + (e.speed || 0) * this.sinD(e.heading || 0), 0
-          );
-          return {
-            speed: Math.sqrt(sumX ** 2 + sumY ** 2) / cache.length,
-            heading: this.atan2D(sumY, sumX),
-          }
+      _smoothMotion: function (e) {
+        this._motionCache.push(e);
+        if (this._motionCache.length > this.options.motionCacheLength) {
+          this._motionCache.shift();
         }
-      })(),
+        const sumX = this._motionCache.reduce(
+          (sum, e) => sum + (e.speed || 0) * cosD(e.heading || 0), 0
+        );
+        const sumY = this._motionCache.reduce(
+          (sum, e) => sum + (e.speed || 0) * sinD(e.heading || 0), 0
+        );
+        return {
+          speed: Math.sqrt(sumX ** 2 + sumY ** 2) / this._motionCache.length,
+          heading: atan2D(sumY, sumX),
+        }
+      },
     })
   }
 
