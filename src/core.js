369c5bc4ab3d3fc04299d31661faf76d0cab1633
clem <clement.dupre@gmail.com>
2026-09-06 22:11:17 +0200

    error

--- src/core.js
+++ src/core.js
@@ -336,6 +336,7 @@ export default function createPlugin(L) {
 
     // public method with default behaviour
     onLocationError: function (e) {
+      console.error(e)
       if (e.code === 1) {
         alert('unlock geolocation please')
       }
